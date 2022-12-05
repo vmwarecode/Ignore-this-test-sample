98cdde7ed0e6cd98469d8ea4f1636792709da4b4
VMware Code <noreply@vmware.com>
2022-12-05 08:37:30 +0000

    Initial commit by Ashiko West

---
+++ snippet.py
@@ -0,0 +1,4 @@
+This is another test.
+This is a test sample on. Please note that it must be at least 200 characters long, 
+or you'll get an error or unexpected behavior. The length is intended to encourage contributors 
+to document their samples for improved usability.
